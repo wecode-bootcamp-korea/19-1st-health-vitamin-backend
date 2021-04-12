a65ee225c55b1bcac591891d8ba9d52f47d25b75
dnjs0718 <mystund@naver.com>
2021-04-12 16:44:24 +0900

    first setting

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '9r4j0=o-49$-dtysgon-&dktyhz(e-0yqkn!h+mlci-$z=@w4l'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'health_and_vitamin',
+        'USER': 'root',
+        'PASSWORD': 'wodus1034!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
