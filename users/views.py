c754009301bd7e3d7975292a5cf82bdf11d6393c
hyungsub <koreanronin@gmail.com>
2021-04-15 20:43:45 +0900

    Add: user 앱의 views.py에 회원가입 class 생성 - 회원가입을 위한 SignUpView 클래스 생성 - KEY ERROR 응답 작성

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http    import JsonResponse
+from django.views   import View
+
+from .models        import User
+
+
+class SignUpView(View):
+    def post(self, request):
+
+        try:
+            data = json.loads(request.body)
+
+            division      = data['division']
+            account       = data['account']
+            password      = data['password']
+            name          = data['name']
+            phone_number  = data['phone_number']
+            email         = data['email']
+            gender        = data['gender']
+            date_of_birth = data['date_of_birth']
+
+            User.objects.create(
+                division      = data['division']
+                account       = data['account']
+                password      = data['password']
+                name          = data['name']
+                phone_number  = data['phone_number']
+                email         = data['email']
+                gender        = data['gender']
+                date_of_birth = data['date_of_birth']
+            )
+
+            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status = 201)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status = 400)
