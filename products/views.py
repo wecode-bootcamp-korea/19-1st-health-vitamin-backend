f2598eb6e027d6fb18c71d0419c97af716d74537
dnjs0718 <mystund@naver.com>
2021-04-15 12:11:42 +0900

    [Update] models.py migrate 완료

--- products/views.py
+++ products/views.py
@@ -10,12 +10,13 @@ from django.db.models import Q
 from .models      import Menu
 
 class MenuView(View):
-    menus = Menu.objects.all()
-    arr = []
-    for menu in menus:
-        my_dict = {
-            'menu_id' : menu.id,
-            'menu_name' : menu.name
-        }
-        arr.append(my_dict)
+    def get(self,request):
+        menus = Menu.objects.all()
+        arr = []
+        for menu in menus:
+            my_dict = {
+                'menu_id' : menu.id,
+                'menu_name' : menu.name
+            }
+            arr.append(my_dict)
         return JsonResponse({'menu' : arr},status = 200)
