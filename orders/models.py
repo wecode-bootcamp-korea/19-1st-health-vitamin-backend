96b5db22188e3e267403eb733cedad23b0bbcb55
ulr0 <ulr0xb@gmail.com>
2021-04-17 16:42:06 +0900

    [Update] models.py 수정

--- orders/models.py
+++ orders/models.py
@@ -24,7 +24,7 @@ class PaymentMethod(models.Model):
         db_table = 'payment_methods'
 
 class Order(models.Model):
-    total                = models.PositiveIntegerField()
+    total                = models.PositiveIntegerField(null=True)
     paid_at              = models.DateTimeField(auto_now_add=True, null=True)
     shipping_information = models.ForeignKey(ShippingInformation, on_delete=models.SET_NULL, null=True)
     user                 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
