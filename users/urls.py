49c866feb743284e4c7fb9ece62b63a650728bbe
dnjs0718 <mystund@naver.com>
2021-04-19 20:31:29 +0900

    [19기 원재연][ADD] users/views.py 내 ReviewView GET 메소드 구현완료

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ReviewView
+
+urlpatterns = [
+    path('/reviews', ReviewView.as_view())
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import json
+import datetime
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from .models import Review,User,ReviewImage
+
+class ReviewView(View):
+    def get(self,request):
+        try:
+            reviews = Review.objects.all()
+            if not reviews.exists():
+                return JsonResponse({'MESSAGE': 'REVIEW_DOES_NOT_EXIST'},status = 404)
+            review_list = []
+            for review in reviews:
+                user_review_image_list=[]
+                for image in review.reviewimage_set.all():
+                    user_review_image_list.append({
+                        'review_image_id' : image.id,
+                        'review_image'    : image.image_url
+                    })
+ 
+                review_list.append({
+                    'review_id'         : review.id,
+                    'product_name'      : review.product.name,
+                    'product_image'     : review.product.image_set.all().first().image_url,
+                    'user_account'      : review.user.account,
+                    'user_age'          : datetime.datetime.today().year - int(str(review.user.date_of_birth)[:4]) +1,
+                    'user_review'       : review.text,
+                    'uploaded_at'       : review.uploaded_at,
+                    'updated_at'        : review.updated_at,
+                    'user_review_image' : user_review_image_list,
+                    'gender'            : review.user.gender
+                })
+        
+            return JsonResponse({'REVIEW' : review_list},status=200)
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'},status = 400)
