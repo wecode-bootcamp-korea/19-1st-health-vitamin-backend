41a9d434c94a015a3e134b37583d097b632a3489
ulr0 <ulr0xb@gmail.com>
2021-04-20 22:41:39 +0900

    [Update/Add]장바구니 API POST 수정 및 GET, DELETE 구현

--- orders/views.py
+++ orders/views.py
@@ -6,72 +6,94 @@ from django.http  import JsonResponse
 from django.db    import transaction
 
 from .models         import User, Order, Cart
-from products.models import Product
+from products.models import Product, ShippingFee
+from utils.decorator import user_check
 
 class CartView(View):
-    @transaction.atomic
+    @user_check
     def post(self, request):
-        STATUS_IN_CART = 1
-
-        user       = request.user
-        data       = json.loads(request.body)
-        product_id = data['id']
-        count      = data['count']
-
-        save_point = transaction.savepoint()
+        IN_CART_STATUS_ID = 1
+        user              = request.user
+        data              = json.loads(request.body)
+        products          = data['products']
 
         try:
-            if not Order.objects.filter(user_id=user.id).exists():
-                order = Order(status_id=STATUS_IN_CART, user_id=user.id)
-                order.save()
+            with transaction.atomic():
+                order, created = Order.objects.get_or_create(user_id = user.id, status_id = IN_CART_STATUS_ID)
 
-            if not Order.objects.filter(user_id=user.id, status_id=STATUS_IN_CART).exists():
-                order = Order(status_id=STATUS_IN_CART, user_id=user.id)
-                order.save()
-
-            order   = Order.objects.filter(user_id=user.id, status_id=STATUS_IN_CART)
-            product = order.first().cart_set.filter(product_id=product_id)
-
-            if product.exists():
-                product        = product.first()
-                product.count += count
-                product.save()
-
-            else:
-                cart = Cart(
-                    product_id = product_id,
-                    count      = count,
-                    order_id   = order.first().id
-                    )
-                cart.save()
-
-            options = data['options']
-            for option in options:
-                product_option = order.first().cart_set.filter(product_id=option['option_id'])
-
-                if product_option.exists():
-                    product_option        = product_option.first()
-                    product_option.count += option['count']
-                    product_option.save()
-
-                else:
-                    cart = Cart(
-                        product_id = option['option_id'],
-                        count      = option['count'],
-                        order_id   = order.first().id
-                        )
+                for product in products:
+                    if Cart.objects.filter(order_id  = order.id, product_id = product['product_id']).exists():
+                        cart        = Cart.objects.get(order_id = order.id, product_id = product['product_id'])
+                        cart.count += product['count']
+                    else:
+                        cart = Cart(order_id = order.id, product_id=product['product_id'], count = product['count'])
                     cart.save()
-
-            transaction.savepoint_commit(save_point)
 
             return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
 
         except JSONDecodeError:
-            transaction.savepoint_rollback(save_point)
             return JsonResponse({'MESSAGE' : 'JSON_DECODE_ERROR'}, status=400)
         except KeyError:
-            transaction.savepoint_rollback(save_point)
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
         except ValueError:
-            transaction.savepoint_rollback(save_point)
+            return JsonResponse({'MESSAGE' : 'VALUE_ERROR'}, status=400)
+
+    @user_check
+    def get(self, request):
+        IN_CART_STATUS_ID = 1
+        user              = request.user
+
+        try:
+            if not Order.objects.filter(user_id=user.id, status_id=IN_CART_STATUS_ID).exists():
+                return JsonResponse({'MESSAGE' : 'NO_PRODUCTS_IN_CART'},status=400)
+
+            order = Order.objects.get(user_id=user.id, status_id=IN_CART_STATUS_ID)
+            carts = order.cart_set.all()
+            
+            product_list = [
+                {
+                    'id'       : cart.product_id,
+                    'count'    : cart.count,
+                    'name'     : cart.product.name,
+                    'price'    : cart.product.price,
+                    'discount' : cart.product.discount.rate,
+                    'image'    : cart.product.image_set.first().image_url,
+                    }
+                for cart in carts]
+
+            shipping_fee = [
+                {
+                    'shipping_fee' : ShippingFee.objects.get(id=1).price,
+                    'minimum_free' : ShippingFee.objects.get(id=1).minimum_free
+                    }
+                ]
+
+            return JsonResponse(
+                {
+                    'MESSAGE'      : 'SUCCESS',
+                    'PRODUCT_LIST' : product_list,
+                    'SHIPPING_FEE' : shipping_fee},
+                    status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE' : 'VALUE_ERROR'}, status=400)
+
+    @user_check
+    def delete(self, request):
+        data     = json.loads(request.body)
+        user     = request.user
+        products = data['products']
+
+        try:
+            order = Order.objects.get(user_id=user.id, status_id=1)
+            for product in products:
+                order.cart_set.get(product_id=product['id']).delete()
+
+            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+        except ValueError:
             return JsonResponse({'MESSAGE' : 'VALUE_ERROR'}, status=400)
