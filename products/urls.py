7c699708ff026ab9964699eea4eef137d2eecdf7
dnjs0718 <mystund@naver.com>
2021-04-20 16:28:53 +0900

    [19기 원재연 checkout branch

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ProductDetailView,ProductlistView,CategoryView
+from .views import ProductDetailView,ProductlistView,CategoryView,MainReviewView,MainBestProductView,HashTagView
 
 urlpatterns = [
     path('/detail/<int:product_id>', ProductDetailView.as_view()),
     path('/category',CategoryView.as_view()),
     path('/<int:sub_category_id>',ProductlistView.as_view()),
-    path('/<int:product_id>', ProductDetailView.as_view())
+    path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/main/review',MainReviewView.as_view()),
+    path('/main/best',MainBestProductView.as_view()),
+    path('/main/hashtag',HashTagView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 import json
+import datetime
 
 from django.views import View
 from django.http  import JsonResponse
 
 from .models import Product, ShippingFee, ShippingFee, Image, Option
+from users.models import Review,ReviewImage
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -106,6 +108,83 @@ class ProductlistView(View):
         
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status = 400)
+
+class MainReviewView(View):
+    def get(self,request):
+        reviews = Review.objects.all().order_by('uploaded_at')[:10]
+        real_review_list=[{
+                'review_id'         : review.id,
+                'product_name'      : review.product.name,
+                'product_image'     : review.product.image_set.all().first().image_url,
+                'user_account'      : review.user.account,
+                'user_age'          : datetime.datetime.today().year - int(str(review.user.date_of_birth)[:4]) +1,
+                'user_review'       : review.text,
+                'uploaded_at'       : review.uploaded_at,
+                'updated_at'        : review.updated_at,
+                'user_review_image' : review.reviewimage_set.filter(review_id=review.id).first().image_url if ReviewImage.objects.filter(review_id=review.id) else '',
+                'gender'            : review.user.gender
+                } for review in reviews]
+
+        return JsonResponse({'REAL_REVIEW' : real_review_list}, status = 200)
+
+class MainBestProductView(View):
+    def get(self,request):
+        products = Product.objects.all().order_by('is_best','stock')[:8]
+        best_product_list=[{
+                    'name'       : product.name,
+                    'price'      : product.price,
+                    'discount'   : Discount.objects.get(id=product.discount_id).rate,
+                    'image'      : Image.objects.filter(product_id=product.id).first().image_url,
+                    'product_id' : product.id
+                    } for product in products]
+
+        return JsonResponse({'BEST_PRODUCT' : best_product_list}, status = 200)
+
+class HashTagView(View):
+    def get(self,request):
+        growth_products = SubCategoryProduct.objects.filter(sub_category=1,product__is_best=1)
+        growth_product_list=[{
+                    'name'       : growth_product.product.name,
+                    'price'      : growth_product.product.price,
+                    'discount'   : Discount.objects.get(id=growth_product.product.discount_id).rate,
+                    'image'      : Image.objects.filter(product_id=growth_product.product.id).first().image_url,
+                    'product_id' : growth_product.product.id 
+                    } for growth_product in growth_products][:3]
+
+        focus_on_products = SubCategoryProduct.objects.filter(sub_category=6,product__is_best=1)
+        focus_on_product_list=[{
+                'name'       : focus_on_product.product.name,
+                'price'      : focus_on_product.product.price,
+                'discount'   : Discount.objects.get(id=focus_on_product.product.discount_id).rate,
+                'image'      : Image.objects.filter(product_id=focus_on_product.product.id).first().image_url,
+                'product_id' : focus_on_product.product.id
+                } for focus_on_product in focus_on_products][:3]
+        
+        skin_products = SubCategoryProduct.objects.filter(sub_category=1,product__is_best=1)
+        skin_product_list=[{
+                'name'       : skin_product.product.name,
+                'price'      : skin_product.product.price,
+                'discount'   : Discount.objects.get(id=skin_product.product.discount_id).rate,
+                'image'      : Image.objects.filter(product_id=skin_product.product.id).first().image_url,
+                'product_id' : skin_product.product.id
+                } for skin_product in skin_products][:3]
+        
+        eye_products = SubCategoryProduct.objects.filter(sub_category=3,product__is_best=1)
+        eye_product_list=[{
+                'name'       : eye_product.product.name,
+                'price'      : eye_product.product.price,
+                'discount'   : Discount.objects.get(id=eye_product.product.discount_id).rate,
+                'image'      : Image.objects.filter(product_id=eye_product.product.id).first().image_url,
+                'product_id' : eye_product.product.id
+                } for eye_product in eye_products][:3]
+
+        return JsonResponse({
+            'HASH_TAG_GROWTH_PRODUCT'   : growth_product_list,
+            'HASH_TAG_FOCUS_ON_PRODUCT' : focus_on_product_list,
+            'HASH_TAG_SKIN_PRODUCT'     : skin_product_list,
+            'HASH_TAG_EYE_PRODUCT'      : eye_product_list}
+            ,status=200
+            )
 
 
 
