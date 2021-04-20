571f70d1f93b2e28e9724edd4c7875d01a679433
dnjs0718 <mystund@naver.com>
2021-04-20 19:34:32 +0900

    [19기 원재연] [UPDATE] change branch

--- db_uploader.py
+++ db_uploader.py
@@ -57,31 +57,31 @@ from orders.models import *
 #         )
 
 
-CSV_PATH_PRODUCTS = './products.csv'
-with open(CSV_PATH_PRODUCTS) as in_file:
-  data_reader = csv.reader(in_file)
-  next(data_reader, None)
-  for row in data_reader:
-      name= row[0]
-      price= row[1]
-      detail= row[2]
-      stock= row[3]
-      expired_at= row[4]
-      is_best= row[5]
-      is_option= row[6]
-      discount = Discount.objects.get(id=row[7])
-      shipping_fee = ShippingFee.objects.get(id=row[8])
-      Product.objects.create(
-          name = name, 
-          price = price, 
-          detail = detail,
-          stock = stock,
-          expired_at = expired_at, 
-          is_best = is_best, 
-          is_option = is_option, 
-          discount=discount,
-          shipping_fee=shipping_fee
-          )
+# CSV_PATH_PRODUCTS = './products.csv'
+# with open(CSV_PATH_PRODUCTS) as in_file:
+#   data_reader = csv.reader(in_file)
+#   next(data_reader, None)
+#   for row in data_reader:
+#       name= row[0]
+#       price= row[1]
+#       detail= row[2]
+#       stock= row[3]
+#       expired_at= row[4]
+#       is_best= row[5]
+#       is_option= row[6]
+#       discount = Discount.objects.get(id=row[7])
+#       shipping_fee = ShippingFee.objects.get(id=row[8])
+#       Product.objects.create(
+#           name = name, 
+#           price = price, 
+#           detail = detail,
+#           stock = stock,
+#           expired_at = expired_at, 
+#           is_best = is_best, 
+#           is_option = is_option, 
+#           discount=discount,
+#           shipping_fee=shipping_fee
+#           )
 
 # CSV_PATH_PRODUCTS = './sub_category_products.csv'
 # with open(CSV_PATH_PRODUCTS) as in_file:
@@ -96,17 +96,17 @@ with open(CSV_PATH_PRODUCTS) as in_file:
 #         )
 
 
-# CSV_PATH_PRODUCTS = './images.csv'
-# with open(CSV_PATH_PRODUCTS) as in_file:
-#   data_reader = csv.reader(in_file)
-#   next(data_reader, None)
-#   for row in data_reader:
-#         image_url=row[0]
-#         product=Product.objects.get(id=row[1])
-#         Image.objects.create(
-#           image_url=image_url,
-#           product=product
-#         )
+CSV_PATH_PRODUCTS = './images.csv'
+with open(CSV_PATH_PRODUCTS) as in_file:
+  data_reader = csv.reader(in_file)
+  next(data_reader, None)
+  for row in data_reader:
+        image_url=row[0]
+        product=Product.objects.get(id=row[1])
+        Image.objects.create(
+          image_url=image_url,
+          product=product
+        )
 
 # CSV_PATH_PRODUCTS = './users.csv'
 # with open(CSV_PATH_PRODUCTS) as in_file:
